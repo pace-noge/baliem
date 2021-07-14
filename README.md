ab27474fd7a0534af9b07b7480832f010b227b82
Nasaruddin <nasaruddin@skorpoints.com>
2021-07-14 09:39:15 +0700

    add README.md

---
+++ README.md
@@ -0,0 +1,55 @@
+#Baliem-keyberon
+
+A hand wired hhkb layout keyboard. it uses [keyberon](https://gtihub.com/Texitoi/keyberon) for its firmware.
+
+![Layout](images/layout.png)
+
+## Material
+  - a STM32F103C8T6 aka blue pill
+  - Acrylic Sandwich case you can find it at dxf folder
+  - 60 Cherry MX compatible keyboard switch
+  - 10 M3 screw
+  - 60 1N4148 diodes
+  - Soldering kit
+
+## KLE json
+this is the raw data for [keyboard layout](http://www.keyboard-layout-editor.com/). 
+```json
+["esc","!\n1","@\n2","#\n3","$\n4","%\n5","^\n6","&\n7","*\n8","(\n9",")\n0","_\n-","+\n=","|\n\\","~\n`"],
+[{w:1.5},"Tab","Q","W","E","R","T","Y","U","I","O","P","{\n[","}\n]",{w:1.5},"Delete"],
+[{w:1.75},"Ctrl","A","S","D","F","G","H","J","K","L",":\n;","\"\n'",{w:2.25},"Return"],
+[{w:2.25},"Shift","Z","X","C","V","B","N","M","<\n,",">\n.","?\n/",{w:1.75},"Shift","Fn"],
+[{x:1.25,w:1.25},"Option",{w:1.25},"⌘",{a:7,w:6.25},"",{a:4,w:1.25},"⌘",{w:1.25},"Option"]
+
+```
+
+## Compiling
+
+Install the rust toolchain
+```shell
+curl https://sh.rustup.rs -sSf | sh
+rustup target add thumbv7m-none-eabi
+rustup component add llvm-tools-preview
+cargo install cargo-binutils
+```
+
+Compile the firmware
+```shell
+cargo objcopy --bin baliem --release -- -O binary baliem.bin
+```
+
+## Flashing using DFU
+
+Put the keyboard into flashing mode. (hit the reset button on the blue pill). Then falsh it:
+```shell
+sudo dfu-util -d 1eaf:0003 -a 2 -D baliem.bin
+```
+
+Push reset and the keyboard was ready to go.
+
+## Notes
+
+reflash bootloader:
+```shell
+openocd -f interface/stlink-v2.cfg -f target/stm32f1x.cfg -c "init; reset halt; stm32f1x mass_erase 0; program generic_boot20_pc13.bin exit 0x08000000"
+```
--- src/layout.rs
+++ src/layout.rs
@@ -12,6 +12,7 @@ const C_ENTER: Action = HoldTap {
     hold: &k(LCtrl),
     tap: &k(Enter),
 };
+
 const L1_SP: Action = HoldTap {
     timeout: 200,
     hold: &l(1),
@@ -19,20 +20,25 @@ const L1_SP: Action = HoldTap {
 };
 const CENTER: Action = m(&[LCtrl, Enter]);
 
+const L1_FN: Action = HoldTap {
+    timeout: 200,
+    hold: &l(1),
+    tap: &k(No)
+};
 
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers = &[
     &[
-        &[k(Escape),    k(Kb1),     k(Kb2),        k(Kb3), k(Kb4),     k(Kb5), k(Kb6), k(Kb7), k(Kb8), k(Kb9),      k(Kb0),   k(Minus),   k(Equal),      k(Bslash),      k(Grave)    ],
-        &[k(Tab),       Trans,           k(Q),          k(W),   k(E),       k(R),   k(T),   k(Y),   k(U),   k(I),        k(O),     k(P),       k(LBracket),   k(RBracket),    k(Delete)   ],
-        &[k(RCtrl),     Trans,           k(A),          k(S),   k(D),       k(F),   k(G),   k(H),   k(J),   k(K),        k(L),     k(SColon),  k(Quote),      k(Return),      Trans            ],
-        &[Trans,             k(LShift),  k(Z),          k(X),   k(C),       k(V),   k(B),   k(N),   k(M),   k(Comma),    k(Dot),   k(Slash),   Trans,              k(RShift),      Trans            ],
-        &[Trans,             k(No),      Trans,              Trans,       Trans,           L1_SP,       Trans,       Trans,       Trans,       k(No),       k(No),    Trans,           Trans,              Trans,               Trans            ],
+        &[k(Escape),    k(Kb1),     k(Kb2),        k(Kb3),  k(Kb4),     k(Kb5),       k(Kb6), k(Kb7), k(Kb8), k(Kb9),      k(Kb0),   k(Minus),   k(Equal),      k(Bslash),      k(Grave)    ],
+        &[k(Tab),       Trans,           k(Q),          k(W),    k(E),       k(R),         k(T),   k(Y),   k(U),   k(I),        k(O),     k(P),       k(LBracket),   k(RBracket),    k(BSpace)   ],
+        &[k(RCtrl),     Trans,           k(A),          k(S),    k(D),       k(F),         k(G),   k(H),   k(J),   k(K),        k(L),     k(SColon),  k(Quote),      k(Return),      Trans            ],
+        &[Trans,             k(LShift),  k(Z),          k(X),    k(C),       k(V),         k(B),   k(N),   k(M),   k(Comma),    k(Dot),   k(Slash),   Trans,              k(RShift),      L1_FN            ],
+        &[Trans,             k(LAlt),    Trans,              k(LGui), Trans,           k(Space),     Trans,       Trans,       Trans,       k(RGui),     k(RAlt),  Trans,           Trans,              Trans,               Trans            ],
     ], &[
-        &[k(F1),        k(F2),      k(F3),         k(F4),  k(F5),      k(F6),  Trans,       Trans,       Trans,       k(F7),       k(F8),    k(F9),      k(F10),        k(F11),        k(F12)       ],
-        &[Trans,             Trans,           Trans,             Trans,        Trans,           Trans,       Trans,       Trans,       Trans,       Trans,            Trans,         k(Delete),  Trans,              Trans,              Trans             ],
-        &[d(0),     d(1),     k(NumLock),   Trans,        k(Escape),  Trans,       Trans,       Trans,       Trans,       k(CapsLock), k(Left),  k(Down),    k(Up),         k(Right),      Trans             ],
-        &[Trans,            Trans,            CUT,               COPY,         PASTE,           Trans,       Trans,       Trans,       Trans,       Trans,            k(Home),  k(PgDown),  k(PgUp),       k(End),        Trans             ],
-        &[Trans,            Trans,            Trans,             Trans,        Trans,           Trans,       Trans,       Trans,        Trans,      Trans,            CENTER,        Trans,           Trans,              Trans,              Trans             ],
+        &[k(F1),        k(F2),      k(F3),         k(F4),  k(F5),       k(F6),        Trans,       Trans,       Trans,       k(F7),       k(F8),    k(F9),      k(F10),        k(F11),         k(F12)       ],
+        &[Trans,             Trans,           Trans,             Trans,        Trans,            Trans,             Trans,       Trans,       Trans,       k(Up),       Trans,         Trans,           Trans,              Trans,               k(Delete)             ],
+        &[d(0),     d(1),     k(NumLock),   Trans,        k(Escape),   Trans,             Trans,       Trans,       k(Left),k(Down),     k(Left),  k(Down),    k(Up),         k(Right),       Trans             ],
+        &[Trans,            Trans,            CUT,               COPY,         PASTE,            Trans,             Trans,       Trans,       Trans,       Trans,            k(Home),  k(PgDown),  k(PgUp),       k(End),         Trans             ],
+        &[Trans,            Trans,            Trans,             Trans,        Trans,            Trans,             Trans,       Trans,        Trans,      Trans,            CENTER,        Trans,           Trans,              Trans,               Trans             ],
     ],
 ];
